5a057877aca449c9536452a65f21be42ac11a8cc
Vivek Singh <vivek.singh.6967@gmail.com>
2025-04-09 14:16:25 +0530

    basic api and graphql integrations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
-mod models;
-mod pokemon_service;
-fn main() {
-    println!("Hello, world!");
+use crate::external::pokemon_api::PokemonService;
+use crate::model::query_root::QueryRoot;
+use crate::routes::graphql::{graphql_handler, graphql_playground};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use axum::{extract::Extension, routing::get, Router};
+// use external::config_loader::{load_config};
+use reqwest::Client;
+use std::{net::SocketAddr, sync::Arc};
+
+mod external;
+mod model;
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    let client = Arc::new(Client::new());
+    let pokemon_service = Arc::new(PokemonService { client });
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let app = Router::new()
+        .route("/", get(PokemonService::check_handler))
+        .route("/graphql", get(graphql_playground).post(graphql_handler))
+        // .route("/check",get(PokemonService::check_handler()))
+        .route(
+            "/debug/fetch_pokemon",
+            get(PokemonService::fetch_pokemon_handler),
+        )
+        .route(
+            "/debug/fetch_evolution",
+            get(PokemonService::fetch_evolution_handler),
+        )
+        // .route(
+        //     "/debug/{*endpoint}",
+        //     get(PokemonService::get_pokemon_handler),
+        // ) // TODO : convert this to internal endpoint
+        .layer(Extension(schema))
+        .layer(Extension(pokemon_service));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    println!("Listening on http://{}", addr);
 }
--- src/models.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize};
-use async_graphql::SimpleObject;
-#[derive(Debug, SimpleObject, Deserialize)]
-pub struct Pokemon{
-    pub name: String,
-    pub hight: i32,
-    pub weight: i32,
-    pub base_experience: i32
-}
