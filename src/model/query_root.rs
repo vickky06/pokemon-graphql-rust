1c6c13662d7ab79d951c8754ef6923d03724dd8d
Vivek Singh <vivek.singh.6967@gmail.com>
2025-04-12 14:26:40 +0530

    update for thread safety

--- src/model/query_root.rs
+++ src/model/query_root.rs
@@ -1,6 +1,8 @@
 use async_graphql::{Context, Object};
 use reqwest::Client;
 use std::sync::Arc;
+use std::panic::AssertUnwindSafe;
+use futures:: FutureExt;
 
 use crate::{
     external::pokemon_api::PokemonService,
@@ -17,51 +19,78 @@ impl QueryRoot {
         "Hello world"
     }
 
-    async fn pokemon<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> Pokemon {
-        match get_dependencies(ctx) {
-            Some((service, client)) => {
-                service
-                    .fetch_pokemon(&name, client)
-                    .await
-                    .unwrap_or_else(|err| {
+
+async fn pokemon<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> Pokemon {
+    match get_dependencies(ctx) {
+        Some((service, client)) => {
+            AssertUnwindSafe(async move {
+                match service.fetch_pokemon(&name, client).await {
+                    Ok(pokemon) => pokemon,
+                    Err(err) => {
                         eprintln!("Error fetching Pokemon: {:?}", err);
                         Pokemon::no_pokemon()
-                    })
-            }
-            None => Pokemon::no_pokemon(),
+                    }
+                }
+            })
+            .catch_unwind()
+            .await
+            .unwrap_or_else(|err| {
+                eprintln!("Error fetching Pokemon: {:?}", err);
+                Pokemon::no_pokemon()
+            })
         }
+        None => Pokemon::no_pokemon(),
     }
+}
 
-    async fn species<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> EvolutionSpecies {
-        match get_dependencies(ctx) {
-            Some((service, client)) => {
-                service
-                    .fetch_species(&name, client)
-                    .await
-                    .unwrap_or_else(|err| {
-                        eprintln!("Error fetching Species: {:?}", err);
-                        EvolutionSpecies::no_pokemon()
-                    })
-            }
-            None => EvolutionSpecies::no_pokemon(),
+async fn species<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> EvolutionSpecies {
+    match get_dependencies(ctx) {
+        Some((service, client)) => {
+            AssertUnwindSafe(async move {
+                match service.fetch_species(&name, client).await{
+                    Ok(pokemon_species) => {
+                        // println!("POKEMON SERVICE: {:?}", pokemon_species);
+                        return pokemon_species;
+                    }
+                    Err(err) => {
+                        println!("{:?}", err);
+                        return EvolutionSpecies::no_pokemon();
+                    }
+                }
+            })
+            .catch_unwind()
+            .await
+            .unwrap_or_else(|err| {
+                eprintln!("Error fetching Species: {:?}", err);
+                EvolutionSpecies::no_pokemon()
+            })
         }
+        None => EvolutionSpecies::no_pokemon(),
     }
+}
 
-    async fn evolution<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> EvolutionFlow {
-        match get_dependencies(ctx) {
-            Some((service, client)) => {
-                service
-                    .fetch_evolution(&name, client)
-                    .await
-                    .unwrap_or_else(|err| {
+async fn evolution<'ctx>(&self, ctx: &Context<'ctx>, name: String) -> EvolutionFlow {
+    match get_dependencies(ctx) {
+        Some((service, client)) => {
+            AssertUnwindSafe(async move {
+                match service.fetch_evolution(&name, client).await{
+                    Ok(evolution_chain) => evolution_chain,
+                    Err(err) => {
                         eprintln!("Error fetching Evolution Chain: {:?}", err);
                         EvolutionFlow::no_evolution_details()
-                    })
-            }
-            None => EvolutionFlow::no_evolution_details(),
+                    }
+                }
+            })
+            .catch_unwind()
+            .await
+            .unwrap_or_else(|err| {
+                eprintln!("Error fetching Evolution Chain: {:?}", err);
+                EvolutionFlow::no_evolution_details()
+            })
         }
+        None => EvolutionFlow::no_evolution_details(),
     }
-
+}
     async fn health(&self, _ctx: &Context<'_>) -> Health {
         Health { is_healthy: true }
     }
